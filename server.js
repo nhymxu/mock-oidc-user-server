04f646e386ab7d9759eb59827d2b11278adc787e
Dung Nguyen <contact@dungnt.net>
2022-12-16 18:35:54 +0700

    mock server

---
+++ server.js
@@ -0,0 +1,41 @@
+const { OAuth2Server } = require('oauth2-mock-server');
+
+const serverPort = process.env.PORT || 8080;
+const issuerURL = process.env.ISSUER_URL || false;
+
+async function startServer() {
+    const optionKey = undefined;
+    const optionCert = undefined;
+    const oauth2Optioons = {
+        'endpoints': {
+            wellKnownDocument: '/.well-known/openid-configuration',
+            token: '/as/token.oauth2',
+            jwks: '/jwks',
+            authorize: '/as/authorization.oauth2',
+            userinfo: '/idp/userinfo.openid',
+            revoke: '/revoke',
+            endSession: '/idp/startSLO.ping',
+            introspect: '/introspect',
+        }
+    }
+    let server = new OAuth2Server(optionKey, optionCert, oauth2Optioons);
+    
+    if (issuerURL) {
+        server.issuer.url = issuerURL;
+    }
+
+    // Generate a new RSA key and add it to the keystore
+    await server.issuer.keys.generate('RS256');
+
+    // Start the server
+    await server.start(serverPort);
+    console.log('Issuer URL:', server.issuer.url); // -> http://localhost:8080
+    
+    // Do some work with the server
+    // ...
+    
+    // Stop the server
+    // await server.stop();
+}
+
+module.exports = startServer();
